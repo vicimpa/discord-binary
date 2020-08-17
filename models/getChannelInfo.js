684fc3f31ad40b255d87ae9c834b2eb4dfa6d28e
PromiSe#### <vicimpa@ya.ru>
2020-08-17 17:32:31 +0300

    Добавил новый модуль, для информации

---
+++ models/getChannelInfo.js
@@ -0,0 +1,81 @@
+const { Client, Channel, VoiceChannel, GroupDMChannel, TextChannel, PermissionOverwrites } = require('discord.js')
+
+exports.title = 'ChannelInfo (Утилитка получения информации о канале)'
+
+function update() {
+  console.log('Добро пожаловать в ChannelInfo')
+  console.log('Просто напишите сюда ID канала')
+}
+
+function toOut(data = '') {
+  console.log(data.trim().split('\n').map(e => e.trim()).filter(e => !!e).join('\n'))
+}
+
+/** @param {PermissionOverwrites} perm */
+function getInfoPermission(perm) {
+  if(perm.type == 'member') {
+    return `
+      MemberName: ${perm?.channel?.client?.users?.get(perm.id)?.username}
+    `.trim()
+  }
+
+  if(perm.type == 'role') {
+    return `
+      RoleName: ${perm?.channel?.guild?.roles?.get(perm.id)?.name}
+    `.trim()
+  }
+ 
+  return ''
+}
+
+/** @param {Channel} channel */
+function getInfoChannel(channel) {
+  toOut(`
+    ID: ${channel.id}
+    Type: ${channel.type}
+    CreatedAt: ${channel.createdAt}
+  `)
+
+  if(channel instanceof VoiceChannel || channel instanceof TextChannel) {
+    toOut(`
+      Name: ${channel.name}
+      Position: ${channel.position}
+      ParentID: ${channel.parentID}
+
+      PERMISSIONS:
+    `)
+
+    toOut(channel.permissionOverwrites.map((value, key) => {
+      return `
+        = ID: ${value.id}
+        = Type: ${value.type}
+        = ${getInfoPermission(value)}
+        = Allow: ${value.allow}
+        = Deny: ${value.deny}
+        =-------------------------
+      `.trim()
+    }).join('\n'))
+  }
+
+
+}
+
+/** @param {Client} client */
+exports.model = async function ClearChat(client) {
+  console.clear()
+  update()
+
+  process.stdin.on('data', async (d) => {
+    try {
+      let find = await client.channels.get(d.toString().trim())
+
+      if (find)
+        return getInfoChannel(find)
+
+      console.log('Канал не найден!')
+    } catch (e) {
+      update()
+    }
+  })
+  process.stdin.resume()
+}
